1467a16033bacbc3305881791bed5e2be3af669b
Banduliak Andrii <109424920+AndriiBanduliak@users.noreply.github.com>
2022-09-27 22:42:17 +0300

    Update task2.py

--- hw2/task2.py
+++ hw2/task2.py
@@ -10,8 +10,7 @@
 n = int(input("Enter the number "))
 
 f = 1
-i = range(2,n+1)
 
-for i in range(2, n+1):
+for i in range(1, n+1):
     f *=i
     print("The factorial of ",i,"the input's"" number is", f)
